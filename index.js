7741d023fa9db5e9427ca9862ff6d451e18e5061
truptijadhav25 <109571554+truptijadhav25@users.noreply.github.com>
2022-07-22 14:06:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+convertFahrenheit = ()=>{
+    var celsius=(document.querySelector("#celsius").value);
+
+    var fahrenheitResult = (1.8 * celsius)+32;
+    var a = fahrenheitResult.toFixed(2);
+
+    document.querySelector("#celsius").value = celsius;
+    document.querySelector("#fahrenheit").value = a;
+}
+
+convertCelsius = ()=>{
+    var fahrenheit = parseFloat(document.querySelector("#fahrenheit").value);
+
+    var celsiusResult = (fahrenheit - 32)/1.8;
+    var b = celsiusResult.toFixed(2);
+
+    document.querySelector("#celsius").value = b;
+    document.querySelector("#fahrenheit").value = fahrenheit;
+}
+
+reset = () =>
+{
+    document.querySelector("#celsius").value = " ";
+    document.querySelector("#fahrenheit").value = " ";
+}
